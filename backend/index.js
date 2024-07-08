7d74abab148fbec446afb89f4e04efe9da13205a
Ammad <ammadq87@my.yorku.ca>
2024-07-08 19:46:41 -0400

    expressjs initialization and .env example

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// ToDo -- Might need to supply professor with these values?
+// Retrieves sensitive values from .env file, I.E.: API Keys, Passwords, etc
+require('dotenv').config();
+
+// Parses JSON body-requests
+const bodyParser = require('body-parser');
+
+// How to add Controller / API Endpoints:
+// const CONTROLLER = require('./controllers/CONTROLLER_NAME')
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// How to add controller to application
+// app.use('/CONTROLLER', CONTROLLER)
+
+app.listen(3001, function(){
+    console.log(`Listening on PORT 3001`);
+});
