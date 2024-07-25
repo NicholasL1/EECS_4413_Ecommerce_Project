119c8fcdf79eaf83b2fa425395d6650bacd0db4f
Ryan L <fartman553@gmail.com>
2024-07-24 20:39:15 -0400

    Change request status to 201

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -87,7 +87,7 @@ router.post("/Register", async (req, res) => {
 app.get("/account/:id", (req, res) => {
   try {
     const userId = req.params.id;
-    const user =  UserService.getUserById(userId);
+    const user = UserService.getUserById(userId);
 
     if (!user) {
       return res.sendStatus(404);
@@ -105,12 +105,22 @@ app.get("/account/:id", (req, res) => {
 
 router.post("/updateUser", async (req, res) => {
   try {
-    const userId = req.params.id;
-    const updateData = req.body;
+    const { userId, updateData } = req.body;
+
+    if (!userId || !updateData) {
+      return res.status(400).json({ message: "ID and update is required" });
+    }
+
     const user = await UserService.updateUser(userId, updateData);
 
-    res.status(200).json({
-      message: "Update Success",
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found in the database" });
+    }
+
+    res.status(201).json({
+      message: "Update Successful",
       user: {
         id: user._id,
         first_name: user.first_name,
@@ -120,7 +130,8 @@ router.post("/updateUser", async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ message: "server error" });
+    console.error("Error updating user:", error);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
