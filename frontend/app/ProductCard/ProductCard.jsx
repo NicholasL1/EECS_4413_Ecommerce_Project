6ae40fd998580ddb671082f4aa982c2aafe94639
ethan <ewmackay@my.yorku.ca>
2024-08-17 21:21:42 -0400

    These got left out of the first commit somehow

---
+++ frontend/app/ProductCard/ProductCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ProductCard.css';
+import img1 from '../../public/4413 Shoe Pics/NIKE+AIR+MAX+270+WHITE+1.png';
+
+const ProductCard = ({product}) => {
+
+    return (
+        <div className='product-card'>
+            <div className='product-img'>
+                <img src={product.image ? product.image : img1} alt={product.name}></img>
+            </div>
+            <div className='product'>
+                <p className='name'>{product.name}</p>
+                <p className='gender'>{product.gender}</p>
+                <p className='price'>${product.price}</p>
+
+            </div>
+        </div>
+    );
+
+}
+
+export default ProductCard;
