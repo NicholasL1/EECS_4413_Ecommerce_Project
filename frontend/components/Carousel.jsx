5d9baa158ddee2173d0d82f247bfc760a93720b8
Nicholas Lachhman <nickla@my.yorku.ca>
2024-08-18 03:20:59 -0400

    Working landing page frontend w/o functionality

---
+++ frontend/components/Carousel.jsx
@@ -0,0 +1,80 @@
+import MensBanner from "@/components/hero/MensBanner";
+import WomensBanner from "@/components/hero/WomensBanner";
+import KidsBanner from "@/components/hero/KidsBanner";
+import SalesBanner from "@/components/hero/SaleBanner";
+import React, { useState, useEffect } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+const slides = [
+  <MensBanner key="mens" />,
+  <WomensBanner key="womens" />,
+  <KidsBanner key="kids" />,
+  <SalesBanner key="sales" />,
+];
+function Carousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  return (
+    <section className="relative px-4 py-16 m-auto w-full max-w-[1600px] h-[780px] rounded-xl">
+      <div className="relative w-full h-full rounded-2xl overflow-hidden group">
+        <div
+          className="flex transition-transform duration-500"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className="flex-shrink-0 w-full h-full bg-center bg-cover"
+            >
+              {slide}
+            </div>
+          ))}
+        </div>
+        {/* left arrow */}
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer hover:bg-black/80">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/* right arrow */}
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer hover:bg-black/80">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`text-2xl cursor-pointer ${
+              currentIndex === index ? "text-custom-red" : "text-black"
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Carousel;
---
+++ frontend/components/Price.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import HighToLow from "@/public/basketball.jpg";
+import LowToHigh from "@/public/soccer.jpg";
+import BestDeals from "@/public/deals.jpg";
+import Link from "next/link";
+
+const priceCardInfo = [
+  {
+    category: "Low to High",
+    link: "/",
+    background: HighToLow,
+    height: "500px",
+  },
+  {
+    category: "High to Low",
+    link: "/",
+    background: LowToHigh,
+    height: "500px",
+  },
+  {
+    category: "Our Best Deals",
+    link: "/",
+    background: BestDeals,
+    height: "300px",
+  },
+];
+
+function PriceCard({ category, link, background, height }) {
+  return (
+    <Link href={link}>
+      <div
+        className={`h-[${height}] flex flex-col items-center justify-center p-4 shadow-lg rounded-lg hover:shadow-xl hover:scale-[1.05] transition-all duration-800 cursor-pointer bg-cover bg-center`}
+        style={{ backgroundImage: `url(${background.src})` }}
+      >
+        <h3 className="text-6xl font-semibold text-white">{category}</h3>
+      </div>
+    </Link>
+  );
+}
+
+export default function Price() {
+  return (
+    <section className="relative px-4 py-16 m-auto w-full max-w-[1700px]">
+      <h1 className="text-3xl font-semibold font-signika-negative underline mb-8">
+        Shop By Price
+      </h1>
+      {/* Three cards */}
+      <div className="flex flex-col">
+        {/* Row 1: Two Cards */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <PriceCard {...priceCardInfo[0]} />
+          <PriceCard {...priceCardInfo[1]} />
+        </div>
+        {/* Row 2: One Card */}
+        <div className="mt-4">
+          <PriceCard {...priceCardInfo[2]} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+//   <div className="flex flex-col lg:flex-row w-full h-full gap-4">
+//   {/* Row 1: Two Cards */}
+//   <div className="flex flex-col lg:flex-row md:flex-row gap-4 w-full">
+//     <Link href={priceCardInfo[0].link}>
+//       <div
+//         className="flex-1 flex flex-col items-center justify-center p-4 shadow-lg rounded-lg hover:shadow-xl hover:scale-[1.05] transition-all duration-800 cursor-pointer bg-cover bg-center"
+//         style={{
+//           backgroundImage: `url(${priceCardInfo[0].background.src})`,
+//         }}
+//       >
+//         <h3 className="text-xl font-semibold text-white">
+//           {priceCardInfo[0].category}
+//         </h3>
+//       </div>
+//     </Link>
+//     <Link href={priceCardInfo[1].link}>
+//       <div
+//         className="flex-1 flex flex-col items-center justify-center p-4 shadow-lg rounded-lg hover:shadow-xl hover:scale-[1.05] transition-all duration-800 cursor-pointer bg-cover bg-center"
+//         style={{
+//           backgroundImage: `url(${priceCardInfo[1].background.src})`,
+//         }}
+//       >
+//         <h3 className="text-xl font-semibold text-white">
+//           {priceCardInfo[1].category}
+//         </h3>
+//       </div>
+//     </Link>
+//   </div>
+//   {/* Row 2: Single Card */}
+//   <div className="w-full mt-4 lg:mt-0">
+//     <Link href={priceCardInfo[2].link}>
+//       <div
+//         className="flex flex-col items-center justify-center p-4 shadow-lg rounded-lg hover:shadow-xl hover:scale-[1.05] transition-all duration-800 cursor-pointer bg-cover bg-center h-[200px]"
+//         style={{
+//           backgroundImage: `url(${priceCardInfo[2].background.src})`,
+//         }}
+//       >
+//         <h3 className="text-xl font-semibold text-white">
+//           {priceCardInfo[2].category}
+//         </h3>
+//       </div>
+//     </Link>
+//   </div>
+// </div>
