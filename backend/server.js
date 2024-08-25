6bbfda24f6df2f377aac9f4a31dd5b36070a7d5f
Nicholas Lachhman <nickla@my.yorku.ca>
2024-08-25 00:01:56 -0400

    updates

--- backend/server.js
+++ backend/server.js
@@ -46,20 +46,6 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  /*
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://6ixkicks-60sgevxeo-nicholasl1s-projects.vercel.app"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-  */
-  // Initialize session variables on the req.session object
   if (!req.session.cart) {
     req.session.cart = {};
   }
