cdacf7462705c3bc5dbc0d9ab307cfd6e220fb20
Nicholas Lachhman <nickla@my.yorku.ca>
2024-08-26 20:03:54 -0400

    fixed for prod

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,15 @@
-// Dependencies
 const express = require("express");
-const dotenv = require("dotenv").config(); // Retrieves sensitive values from .env file, I.E.: API Keys, Passwords, etc
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const memoryStore = new session.MemoryStore();
 
-// Config
 const connectDB = require("./config/db");
 
-// Port env variable
 const PORT = process.env.PORT || 3001;
 
-// Controllers
-// const CONTROLLER = require('./controllers/CONTROLLER_NAME')
 const User = require("./controllers/UserController.js");
 const Cart = require("./controllers/CartController.js");
 const Order = require("./controllers/OrderController.js");
@@ -23,28 +18,22 @@ const Product = require("./controllers/ProductController.js");
 const Admin = require("./controllers/AdminController.js");
 const Review = require("./controllers/ReviewController.js");
 
-// Connect to database
 connectDB();
 
 const app = express();
 
-app.use(bodyParser.json({limit: '5mb' }));
-app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
 app.use(
   cors({
-    origin: "https://6ixkicks.vercel.app", // Allow requests from your frontend
-    credentials: true, // Allow cookies and other credentials
-    methods: "GET, POST, PUT, DELETE, OPTIONS, PATCH", // Specify allowed methods
+    origin: /^https:\/\/6ixkicks\.vercel\.app$/,
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
     allowedHeaders:
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization", // Specify allowed headers
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   })
 );
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 app.use(
@@ -55,12 +44,11 @@ app.use(
     store: memoryStore,
     cookie: {
       httpOnly: true,
+      secure: true,
       sameSite: "lax",
     },
   })
 );
-
-app.options("*", cors());
 
 app.use((req, res, next) => {
   res.setHeader("Referrer-Policy", "no-referrer");
@@ -73,18 +61,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   if (!req.sessionStore.cart) {
-//     req.sessionStore.cart = {};
-//   }
-//   if (req.sessionStore.loggedIn === undefined) {
-//     req.sessionStore.loggedIn = false;
-//   }
-//   next();
-// });
-
-// How to add controller to application
-// app.use('/CONTROLLER', CONTROLLER)
 app.use("/User", User);
 app.use("/Cart", Cart);
 app.use("/Order", Order);
@@ -97,6 +73,12 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to shoe store" });
 });
 
-app.listen(PORT, function () {
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
